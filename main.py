866157164045aee6d1622bcaa930abc20f50c854
Sondos Mohamed <39069270+So47@users.noreply.github.com>
2024-01-12 14:46:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+
         count = 0
         for i in range(len(nums)):
-            if(nums[i] != val):
+            if nums[i] != val:
+                nums[count]= nums[i] # update in place and removing val
+                count += 1
                 print(count,i)
-                nums[count] = nums[i]
-                count+=1
+            else:
+                nums[i]= None
+
         print(nums)        
         return count        
-        
